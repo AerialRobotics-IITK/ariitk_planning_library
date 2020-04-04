7d5d120bc18807a9dc8ebb4746527fd92139a458
rr <gajendra.nagar@rapyuta-robotics.com>
2020-04-05 01:49:32 +0900

    node fix

--- frontier_explorer/src/frontier_evaluator_node.cpp
+++ frontier_explorer/src/frontier_evaluator_node.cpp
@@ -18,8 +18,7 @@ int main(int argc, char** argv) {
     
     while(ros::ok()) {
         ros::spinOnce();
-        evaluator.visualizeVoxelStates();
-        evaluator.visualizeFrontiers();
+        evaluator.run();
         loop_rate.sleep();
     }
     
