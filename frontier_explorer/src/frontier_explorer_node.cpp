d385cd78c2ea930637a3d00fcb48174be6834803
Ashwin Shenai <ashwins@iitk.ac.in>
2020-04-09 18:24:04 +0530

    renamed node file

---
+++ frontier_explorer/src/frontier_explorer_node.cpp
@@ -0,0 +1,26 @@
+#include <frontier_explorer/frontier_evaluator.hpp>
+
+using namespace ariitk::frontier_explorer;
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "frontier_evaluator");
+    google::InitGoogleLogging(argv[0]);
+    google::InstallFailureSignalHandler();
+
+    ros::NodeHandle nh("");
+    ros::NodeHandle nh_private("~");
+
+    FLAGS_alsologtostderr = true;
+
+    FrontierEvaluator evaluator(nh, nh_private);
+
+    ros::Rate loop_rate(10);
+    
+    while(ros::ok()) {
+        ros::spinOnce();
+        evaluator.run();
+        loop_rate.sleep();
+    }
+    
+    return 0;
+}
