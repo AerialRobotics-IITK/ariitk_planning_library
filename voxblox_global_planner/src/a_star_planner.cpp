652d9290b1c9b917128b11f9283e737c06446633
BangYourGang <dvyanshu@iitk.ac.in>
2020-05-03 23:59:38 +0530

    Some cleanup

--- voxblox_global_planner/src/a_star_planner.cpp
+++ voxblox_global_planner/src/a_star_planner.cpp
@@ -23,20 +23,6 @@ AStarPlanner::AStarPlanner(const ros::NodeHandle& nh,
 // }
 
 void AStarPlanner::esdfSliceCallback(sensor_msgs::PointCloud2 pointcloud) {
-  // pointcloud_.header.seq = pointcloud.header.seq;
-  // pointcloud_.header.stamp = pointcloud.header.stamp;
-  // pointcloud_.header.frame_id = pointcloud.header.frame_id;
-  // pointcloud_.height = pointcloud.height;
-  // pointcloud_.width = pointcloud.width;
-  // pointcloud_.fields.name = pointcloud.fields.name;
-  // pointcloud_.fields.offset = pointcloud.fields.offset;
-  // pointcloud_.fields.datatype = pointcloud.fields.datatype;
-  // pointcloud_.fields.count = pointcloud.fields.count;
-  // pointcloud_.is_bigendian = pointcloud.is_bigendian;
-  // pointcloud_.point_step = pointcloud.point_step;
-  // pointcloud_.row_step = pointcloud.row_step;
-  // pointcloud_.data = pointcloud.data;
-  // pointcloud_.is_dense = pointcloud.is_dense;
   pointcloud_ = pointcloud;
 }
 
