74ab273e53de9abbe5dcb74c70ab24c5444cd597
BangYourGang <dvyanshu@iitk.ac.in>
2020-05-07 18:58:30 +0530

    added support for visualization

--- voxblox_global_planner/include/voxblox_global_planner/a_star_planner.hpp
+++ voxblox_global_planner/include/voxblox_global_planner/a_star_planner.hpp
@@ -4,7 +4,6 @@
 
 #include <ros/ros.h>
 #include <voxblox_ros/esdf_server.h>
-#include <voxblox_skeleton/skeleton_generator.h>
 #include <Eigen/Eigen>
 // #include <mav_planning_msgs/PlannerService.h>
 // #include <std_srvs/Empty.h>
@@ -12,6 +11,10 @@
 #include <boost/geometry.hpp>
 #include <boost/geometry/geometries/point.hpp>
 #include <boost/geometry/index/rtree.hpp>
+#include <std_msgs/ColorRGBA.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <visualization_msgs/MarkerArray.h>
+#include <mav_msgs/eigen_mav_msgs.h>
 
 namespace ariitk::global_planner {
 
@@ -33,6 +36,62 @@ struct GraphNode {
 
 typedef std::shared_ptr<struct GraphNode> Ptr;
 typedef GraphNode::Ptr Node;
+typedef std::vector<Node> Graph;
+
+class Color : public std_msgs::ColorRGBA {
+    public:
+        Color() : std_msgs::ColorRGBA() {}
+        Color(double red, double green, double blue) : Color(red, green, blue, 1.0) {}
+        Color(double red, double green, double blue, double alpha) : Color() {
+            r = red;
+            g = green;
+            b = blue;
+            a = alpha;
+
+        }
+
+    static const Color White() { return Color(1.0, 1.0, 1.0); }
+    static const Color Black() { return Color(0.0, 0.0, 0.0); }
+    static const Color Gray() { return Color(0.5, 0.5, 0.5); }
+    static const Color Red() { return Color(1.0, 0.0, 0.0); }
+    static const Color Green() { return Color(0.0, 1.0, 0.0); }
+    static const Color Blue() { return Color(0.0, 0.0, 1.0); }
+    static const Color Yellow() { return Color(1.0, 1.0, 0.0); }
+    static const Color Orange() { return Color(1.0, 0.5, 0.0); }
+    static const Color Purple() { return Color(0.5, 0.0, 1.0); }
+    static const Color Chartreuse() { return Color(0.5, 1.0, 0.0); }
+    static const Color Teal() { return Color(0.0, 1.0, 1.0); }
+    static const Color Pink() { return Color(1.0, 0.0, 0.5); }
+};
+
+class PathVisualizer {
+    public:
+        void init(const ros::NodeHandle& nh, const ros::NodeHandle& nh_private);
+        void createPublisher(const std::string& topic_name);
+        enum class ColorType{WHITE, BLACK, GRAY, RED, GREEN, BLUE, YELLOW, ORANGE, PURPLE, CHARTREUSE, TEAL, PINK};
+        void visualizePath(const std::string& topic_name, const std::vector<Eigen::Vector3d>& path, 
+                       const std::string& frame_id = "world", const ColorType& color = ColorType::PINK, const double& size_factor = 0.5);
+        void visualizePaths(const std::string& topic_name, const std::vector<std::vector<Eigen::Vector3d>>& paths, 
+                       const std::string& frame_id = "world", const ColorType& color = ColorType::PINK, const double& size_factor = 0.5);
+        void visualizeGraph(const std::string& topic_name, const Graph& graph, const std::string& frame_id = "world",
+                        const ColorType& vertex_color = ColorType::ORANGE, const ColorType& edge_color = ColorType::BLUE, const double& size_factor = 0.5);
+        void visualizePoint(const std::string& topic_name, const Eigen::Vector3d& point, 
+                       const std::string& frame_id = "world", const ColorType& color = ColorType::RED, const double& size_factor = 1.0);
+        void visualizePoints(const std::string& topic_name, const std::vector<Eigen::Vector3d>& point, 
+                       const std::string& frame_id = "world", const ColorType& color = ColorType::RED, const double& size_factor = 1.0);
+        void visualizeTrajectory(const std::string& topic_name, const mav_msgs::EigenTrajectoryPointVector& trajectory, 
+                       const std::string& frame_id = "world", const ColorType& color = ColorType::BLACK, const double& size_factor = 1.0);
+
+    private:
+        ros::NodeHandle nh_;
+        ros::NodeHandle nh_private_; 
+
+        bool visualize_;
+        double voxel_size_;
+
+        std::unordered_map<ColorType, Color> color_map_;
+        std::unordered_map<std::string, ros::Publisher> publisher_map_;
+};
 
 class AStarPlanner {
     public:
@@ -60,7 +119,8 @@ class AStarPlanner {
     
         double robot_radius_;
         RTree tree_;
+        PathVisualizer visualizer_;
+        bool visualize_;
 };
-
 
 }
--- voxblox_global_planner/src/a_star_planner.cpp
+++ voxblox_global_planner/src/a_star_planner.cpp
@@ -12,10 +12,11 @@ GraphNode::GraphNode(const pcl::PointXYZI& point,uint id) {
 AStarPlanner::AStarPlanner(const ros::NodeHandle& nh,
                           const ros::NodeHandle& nh_private)
   : nh_(nh),
-  nh_private_(nh_private),
-  voxblox_server_(nh_,nh_private_) {
+    nh_private_(nh_private),
+    voxblox_server_(nh_,nh_private_) {
 
     nh_private_.param("robot_radius",robot_radius_,0.45);
+    nh_private_.param("visualize",visualize_,true);
 
     esdf_slice_sub_ = nh_private_.subscribe("esdf_slice",1,&AStarPlanner::esdfSliceCallback,this);
     esdf_slice_pub_ = nh_private_.advertise<pcl::PointCloud<pcl::PointXYZI> >("esdf_slice_out",1,true);
@@ -51,11 +52,97 @@ void AStarPlanner::generateGraph() {
     }
 }
 
+void PathVisualizer::init(const ros::NodeHandle& nh, const ros::NodeHandle& nh_private) {
+    nh_ = nh; nh_private_ = nh_private;
+    
+    nh_private_.getParam("visualize", visualize_);
+    nh_private_.getParam("voxel_size", voxel_size_);
+
+    color_map_.insert(std::make_pair(PathVisualizer::ColorType::WHITE,      Color::White()));
+    color_map_.insert(std::make_pair(PathVisualizer::ColorType::BLACK,      Color::Black()));
+    color_map_.insert(std::make_pair(PathVisualizer::ColorType::BLUE,       Color::Blue()));
+    color_map_.insert(std::make_pair(PathVisualizer::ColorType::ORANGE,     Color::Orange()));
+    color_map_.insert(std::make_pair(PathVisualizer::ColorType::YELLOW,     Color::Yellow()));
+    color_map_.insert(std::make_pair(PathVisualizer::ColorType::RED,        Color::Red()));
+    color_map_.insert(std::make_pair(PathVisualizer::ColorType::PINK,       Color::Pink()));
+    color_map_.insert(std::make_pair(PathVisualizer::ColorType::GREEN,      Color::Green()));
+    color_map_.insert(std::make_pair(PathVisualizer::ColorType::GRAY,       Color::Gray()));
+    color_map_.insert(std::make_pair(PathVisualizer::ColorType::TEAL,       Color::Teal()));
+    color_map_.insert(std::make_pair(PathVisualizer::ColorType::CHARTREUSE, Color::Chartreuse()));
+    color_map_.insert(std::make_pair(PathVisualizer::ColorType::PURPLE,     Color::Purple()));
+}
+
+void PathVisualizer::visualizeGraph(const std::string& topic_name, const Graph& graph,
+                        const std::string& frame_id, const ColorType& vertex_color, const ColorType& edge_color, const double& size_factor) {
+  if(graph.empty()) {return;}
+
+  visualization_msgs::MarkerArray markers;  
+  visualization_msgs::Marker vertex_marker;
+
+  vertex_marker.header.frame_id = frame_id;
+  vertex_marker.header.stamp = ros::Time::now();
+  vertex_marker.ns = "vertices";
+  vertex_marker.id = 0;
+  vertex_marker.type = visualization_msgs::Marker::CUBE_LIST;
+  vertex_marker.scale.x = vertex_marker.scale.y = vertex_marker.scale.z = voxel_size_ * 0.1;
+  vertex_marker.action = visualization_msgs::Marker::ADD;
+  vertex_marker.color = color_map_[vertex_color];
+
+      for(auto& node : graph) {
+        geometry_msgs::Point point;
+        Eigen::Vector3d pos = node->position_;
+        point.x = pos.x();
+        point.y = pos.y();
+        point.z = pos.z();
+        vertex_marker.points.push_back(point);
+    }
+    markers.markers.push_back(vertex_marker);
+
+    visualization_msgs::Marker edge_marker;
+    edge_marker.header.frame_id = frame_id;
+    edge_marker.header.stamp = ros::Time::now();
+    edge_marker.ns = "edges";
+    edge_marker.id = 0;
+    edge_marker.type = visualization_msgs::Marker::LINE_LIST;
+    edge_marker.scale.x = edge_marker.scale.y = edge_marker.scale.z = 0.005;
+    edge_marker.action = visualization_msgs::Marker::ADD;
+    edge_marker.color = color_map_[edge_color];
+
+    for(auto& node : graph) {
+        geometry_msgs::Point start_point;
+        Eigen::Vector3d start_pos = node->position_;
+
+        start_point.x = start_pos.x();
+        start_point.y = start_pos.y();
+        start_point.z = start_pos.z();
+
+        for(auto& neigh : node->neighbours_) {
+            geometry_msgs::Point end_point;
+            Eigen::Vector3d end_pos = neigh->position_;
+
+            end_point.x = end_pos.x();
+            end_point.y = end_pos.y();
+            end_point.z = end_pos.z();
+
+            edge_marker.points.push_back(start_point);
+            edge_marker.points.push_back(end_point);
+        }
+    }
+    markers.markers.push_back(edge_marker);
+
+    publisher_map_[topic_name].publish(markers);
+}
+
 void AStarPlanner::esdfSliceCallback(pcl::PointCloud<pcl::PointXYZI> pointcloud) {
 
   pointcloud_ = pointcloud;
 
    generateGraph();              //generating graph from Slices
+
+  if(visualize_) {
+    visualizer_.init(nh_,nh_private_);
+    visualizer_.visualizeGraph("graph",graph_);
+  }
 
 }
 
